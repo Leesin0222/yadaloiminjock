40b29aea7cef50a5efde97ff18e347ec1024ffe5
Leesin0222 <dragon02229@naver.com>
2021-10-30 22:45:10 +0900

    🔧 :: 뷰바인딩 코드 추가 및 그리드뷰어댑터 연결

--- bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/MainActivity.kt
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/MainActivity.kt
@@ -4,18 +4,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.viewpager.widget.ViewPager
+import com.yongjincompany.bedaloiminjock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     internal lateinit var viewpager: ViewPager
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val viewpager = findViewById(R.id.viewpager) as ViewPager
 
         val adapter = ViewPagerAdapter(this)
         viewpager.adapter = adapter
+
+        val img = arrayOf(
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner,
+            R.drawable.semicolonbanner
+        )
+
+        val txt = arrayOf(
+            "프론트엔드",
+            "백엔드",
+            "안드로이드",
+            "ios",
+            "게임",
+            "임베디드",
+            "인공지능",
+            "보안"
+        )
+
+        val gridviewAdapter = GridviewAdapter(this, img, txt)
+        binding.gridview.adapter = gridviewAdapter
     }
 }
