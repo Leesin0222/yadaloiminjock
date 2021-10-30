7465d55039a386703f40cb455684ce5bae60718a
Leesin0222 <dragon02229@naver.com>
2021-10-30 22:43:24 +0900

    📑 :: LectureListActivity , xml 생성

---
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
@@ -0,0 +1,11 @@
+package com.yongjincompany.bedaloiminjock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LectureListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lecture_list)
+    }
+}
