69e82cf07edee5645dcd2b1c6590d211b6e7334d
Leesin0222 <dragon02229@naver.com>
2021-11-01 08:31:53 +0900

    🔧 :: Adapter과 연결 해줌

--- bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
@@ -1,11 +1,18 @@
 package com.yongjincompany.bedaloiminjock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.bedaloiminjock.Fragment.FragmentAdapter
+import kotlinx.android.synthetic.main.activity_lecture_list.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LectureListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lecture_list)
+
+        val fragmentAdapter = FragmentAdapter(supportFragmentManager)
+        list_viewpager.adapter = fragmentAdapter
     }
 }
