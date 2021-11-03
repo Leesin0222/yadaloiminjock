0b5f45b25ab8e7371081e006629fef8da24d4985
Leesin0222 <dragon02229@naver.com>
2021-11-04 00:08:15 +0900

    🔧 :: item 의 모델 완성(2)

--- bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/Fragment/ContentsListModel.kt
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/Fragment/ContentsListModel.kt
@@ -1,2 +1,9 @@
 package com.yongjincompany.bedaloiminjock.Fragment
 
+data class ContentsListModel(
+
+    var image : String,
+    var title : String,
+    var number_: Int,
+    var category : String
+)
