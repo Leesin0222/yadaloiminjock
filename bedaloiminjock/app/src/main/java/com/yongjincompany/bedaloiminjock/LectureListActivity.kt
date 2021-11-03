db3979bf37e4b4979c63ac66e13923cb3a3dd79e
Leesin0222 <dragon02229@naver.com>
2021-11-03 12:19:55 +0900

    🔧 :: viewpager 클릭시 Fragment가 하나씩 나오도록

--- bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
@@ -3,9 +3,11 @@ package com.yongjincompany.bedaloiminjock
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import com.google.android.material.tabs.TabLayout
 import com.yongjincompany.bedaloiminjock.Fragment.FragmentAdapter
 import kotlinx.android.synthetic.main.activity_lecture_list.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,6 +26,25 @@ class LectureListActivity : AppCompatActivity() {
         tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("임베")))
         tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("AI")))
         tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("보안")))
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                 Log.e("c", p0.toString())
+
+                if(p0 != null) {
+                    list_viewpager.currentItem = p0.position
+                }
+            }
+
+        })
     }
 
     private fun createTabView(tabName: String): View {
