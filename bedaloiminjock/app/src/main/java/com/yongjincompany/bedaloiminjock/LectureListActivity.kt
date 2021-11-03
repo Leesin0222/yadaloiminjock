50d1bde04aa6214d668bc40df222fa2d9e7ed4f0
Leesin0222 <dragon02229@naver.com>
2021-11-03 11:02:17 +0900

    addtab, createTabView 함수 만듬

--- bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
+++ bedaloiminjock/app/src/main/java/com/yongjincompany/bedaloiminjock/LectureListActivity.kt
@@ -3,6 +3,9 @@ package com.yongjincompany.bedaloiminjock
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import com.yongjincompany.bedaloiminjock.Fragment.FragmentAdapter
 import kotlinx.android.synthetic.main.activity_lecture_list.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,5 +17,23 @@ class LectureListActivity : AppCompatActivity() {
 
         val fragmentAdapter = FragmentAdapter(supportFragmentManager)
         list_viewpager.adapter = fragmentAdapter
+
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("웹")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("앱")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("게임")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("임베")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("AI")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("보안")))
+    }
+
+    private fun createTabView(tabName: String): View {
+
+        val tabView = LayoutInflater.from(baseContext).inflate(R.layout.custom_tab, null)
+
+        val txt_name = tabView.findViewById(R.id.txt_name) as TextView
+
+        txt_name.text = tabName
+
+        return tabView
     }
 }
